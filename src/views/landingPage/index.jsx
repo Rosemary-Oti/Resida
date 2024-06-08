7d07964df79a94fc27f6e50dded46a287fab68bc
Rosemary-Oti <otirosemary58@gmail.com>
2024-06-08 18:34:20 +0100

    InfoCard section

--- src/views/landingPage/index.jsx
+++ src/views/landingPage/index.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from 'shared/header'
 import Hero from 'components/heroSection'
 import SubHeroSection from 'components/subHeroSection'
+import InfoCard from 'components/infocard'
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <Hero />
       <SubHeroSection />
+      <InfoCard />
     </div>
   )
 }
