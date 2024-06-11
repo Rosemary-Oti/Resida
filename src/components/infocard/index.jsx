aa6a39fafd5da986cb201de170f2110757ec91df
Rosemary-Oti <otirosemary58@gmail.com>
2024-06-11 16:32:53 +0100

    completed comprehensive neighborhood guide

--- src/components/infocard/index.jsx
+++ src/components/infocard/index.jsx
@@ -1,28 +1,65 @@
 import React from 'react'
 import './infocard.css'
-import info_image from 'assets/resida-info.svg'
+import Button from 'shared/button'
+import { GoArrowRight } from "react-icons/go";
+// import info_image from 'assets/resida-info.svg'
 
-const InfoCard = () => {
+function InfoCard(props) {
+    const { title, subtitle, body, image, imageStyle, imageHeight, button, infoStyle } = props;
   return (
-    <div className='info_container'>
+<div className='info_container'>
         <div className='info_inner_container'>
-            <div className='info_image'>
-                <img src={info_image} alt="info-image" className='image'/>
+            <div className={imageStyle}>
+                <img src={image} alt="info-image" className={imageHeight}/>
             </div>
             <div className='info_description'>
-                <div className='info'>
-                <h2 className='info_title'>Resida is Not Just an App</h2>
-                <h2 className='info_subtitle'>We are an ecosystem with you at the center</h2>
+                <div className={infoStyle}>
+                <h2 className='info_title'>{title}</h2>
+                <h2 className='info_subtitle'>{subtitle}</h2>
                 <p className='info_body'>
-                    Resida aims to bridge the gap between potential tenants and 
-                    available properties. This app is not just a tool; it's a solution 
-                    for individuals and families who are navigating the challenging
-                    terrain of apartment hunting in a new or familiar city.
+                    {body}
                 </p>
+                {button ? (
+                <Button 
+                style={{
+            width: "195px",
+            color: '#1F4B43',
+            padding: "7px 12px",
+            backgroundColor: '#DBA94D'
+            
+           }} btnName={"Download our app"} 
+           rightIcon={<GoArrowRight color='#1F4B43' size={25}/>}
+           />
+           ) : (
+            ""
+           )}
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
+    // <div className='info_container'>
+    //     <div className='info_inner_container'>
+    //         <div className='info_image'>
+    //             <img src={info_image} alt="info-image" className='image'/>
+    //         </div>
+    //         <div className='info_description'>
+    //             <div className='info'>
+    //             <h2 className='info_title'>Resida is Not Just an App</h2>
+    //             <h2 className='info_subtitle'>We are an ecosystem with you at the center</h2>
+    //             <p className='info_body'>
+    //                 Resida aims to bridge the gap between potential tenants and 
+    //                 available properties. This app is not just a tool; it's a solution 
+    //                 for individuals and families who are navigating the challenging
+    //                 terrain of apartment hunting in a new or familiar city.
+    //             </p>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
   )
 }
 
--- src/views/landingPage/index.jsx
+++ src/views/landingPage/index.jsx
@@ -6,15 +6,37 @@ import SubHeroSection from 'components/subHeroSection'
 import InfoCard from 'components/infocard'
 import OurExperience from 'components/ourexperience'
 import FindYourSpace from 'components/findyourspace'
+import info_image from 'assets/resida-info.svg'
+import guidemap from 'assets/guidemap.svg'
+
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <Hero />
       <SubHeroSection />
-      <InfoCard />
+      {/* InfoCard section */}
+      <InfoCard 
+      title="Resida is Not Just an App" 
+      subtitle="We are an ecosystem with you at the center" 
+      body="Resida aims to bridge the gap between potential tenants and available properties. This app is not just a tool; it's a solution for individuals and families who are navigating the challenging terrain of apartment hunting in a new or familiar city."
+      image={info_image}
+      imageStyle={"info_image"}
+      imageHeight={"image"}
+      infoStyle={"info"}
+      />
       <OurExperience />
       <FindYourSpace />
+      <InfoCard 
+      title="Comprehensive"
+      subtitle="Neighborhood Guides"
+      body="The 'Neighborhood Guides' in Resida are like your friendly local experts, giving you the inside scoop on the vibe, hotspots, and quirks of each area so you can find your perfect match effortlessly! 🏡✨"
+      image={guidemap}
+      imageStyle={"info_comprehensive"}
+      imageHeight={"info_comprehensive_imageheight"}
+      button={true}
+      infoStyle={"info_guide"}
+      />
     </div>
   )
 }
